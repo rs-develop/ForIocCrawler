938b6492312b0fb18163f8a8c712be06dc59b75c
rs-develop <rsdevelop.contact@gmail.com>
2021-04-03 21:22:00 +0200

    Fixed pip install

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ForIocCrawler",
+    version="1.0",
+    author="rs-develop",
+    author_email="rsdevelop.contact@gmail.com",
+    description="A forensic ioc extractor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rs-develop/ForIocCrawler",
+    project_urls={
+        "Bug Tracker": "https://github.com/rs-develop/ForIocCrawler/issues",
+    },
+    scripts=['forioccrawler'],
+    packages=['crawler'],
+	include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8"
+)
